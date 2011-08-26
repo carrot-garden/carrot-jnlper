a26c95a76fffc7073cc414452818770e3a92b39c
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2011-08-26 16:55:27 -0500

    a

--- carrot-jdk6-jnlp-zoom/src/main/java/bench/MainJavaWS.java
+++ carrot-jdk6-jnlp-zoom/src/main/java/bench/MainJavaWS.java
@@ -3,7 +3,12 @@ package bench;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sun.deploy.cache.Cache;
+import com.sun.deploy.cache.LocalApplicationProperties;
 import com.sun.javaws.LocalInstallHandler;
+import com.sun.javaws.jnl.InformationDesc;
+import com.sun.javaws.jnl.LaunchDesc;
+import com.sun.javaws.jnl.LaunchSelection;
 
 public class MainJavaWS {
 
@@ -22,6 +27,21 @@ public class MainJavaWS {
 
 		log.debug("isLocalInstallSupported : {}",
 				install.isLocalInstallSupported());
+
+		InformationDesc info = new InformationDesc(null, null, null, null,
+				null, null, null, null, false);
+
+		LaunchDesc desc = new LaunchDesc("0.1", null, null, null, info,
+				LaunchDesc.ALLPERMISSIONS_SECURITY, null, null,
+				LaunchDesc.INTERNAL_TYPE, null, null, null, null, null, null,
+				null, LaunchSelection.createDefaultMatchJRE());
+
+		String path = "";
+
+		LocalApplicationProperties props = Cache
+				.getLocalApplicationProperties(path);
+
+		install.installShortcuts(desc, props);
 
 	}
 
